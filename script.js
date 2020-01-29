91c30ab6752ee65fd5ce377f043e50682872e590
KonMr <59928352+KonMr@users.noreply.github.com>
2020-01-29 18:32:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-require (["esri/Map", "esri/views/MapView", "esri/WebMap", "esri/geometry/support/webMercatorUtils"],
-   function(Map, MapView, WebMap)
+require (["esri/Map", "esri/views/MapView", "esri/WebMap", "esri/geometry/support/webMercatorUtils", "esri/widgets/BasemapToggle",
+"esri/widgets/BasemapGallery"],
+   function(Map, MapView, WebMap, webMercatorUtils, BasemapToggle, BasemapGallery)
    {
     let map1 = new Map({basemap:"topo"});
     let map2 = new Map({basemap:"satellite"});
     let map3 = new Map({basemap:"osm"});
     let map4 = new WebMap({
         portalItem: {
-            id: "36c9582cc9d140229b9cffb4cd8fe297"
+            id: "aabc0528c401489d97fd0e473a98a2a6"
          }
     });
 
     let mapContainer = new MapView({
-        container: "mapid",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
-        map: map4  //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
-     });
-     
+        container: "mapid",
+        map: map4 
+        
+      });
     
     document.getElementById("przycisk").onclick = function() {
         console.log('klik');
@@ -31,5 +32,10 @@ require (["esri/Map", "esri/views/MapView", "esri/WebMap", "esri/geometry/suppor
     document.getElementById("przycisk4").onclick = function() {
         console.log('klik');
         mapContainer.map = map4;
-    }; 
-   })
+    };
+    mapContainer.on("click", function(evt){
+      let cords = webMercatorUtils.webMercatorToGeographic(evt.mapPoint);
+      console.log(cords.x);
+      console.log(cords.y);
+  });
+   });
